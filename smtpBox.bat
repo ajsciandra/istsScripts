41a6946c8e8473da321a06c0b94bbcf4c6aefa77
ajsciandra <andrewsciandra@comcast.net>
2019-02-22 23:40:09 -0500

    Add More

--- smtpBox.bat
+++ smtpBox.bat
@@ -1,6 +1,14 @@
 netsh advfirewall export "C:\originalFirewall.wfw"
 netsh advfirewall set allprofiles state off
 netsh advfirewall set currentprofile firewallpolicy blockinbound,blockoutbound
-netsh advfirewall firewall add rule name="SMTP Rule" protocol=TCP dir=in localport=587 action=allow
-netsh advfirewall set currentprofile firewallpolicy allowinbound,blockoutbound
+netsh advfirewall firewall add rule name="SMTP" protocol=TCP dir=in localport=587 action=allow
+netsh advfirewall firewall add rule name="BlockOut" dir=out action=block
+netsh advfirewall set currentprofile firewallpolicy allowinbound,allowoutbound
+netsh advfirewall set allprofiles state on
+netsh advfirewall firewall add rule name="All ICMP V4" dir=in action=block protocol=icmpv4
 netsh advfirewall export "C:\goodFirewallRules.wfw"
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+powershell
+Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol
+exit
+shutdown /r
